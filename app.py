4ee77d9272dff1af61c43edaea04459fab57661a
anly22 <alysokhmara@gmail.com>
2024-02-23 12:20:37 +0200

    Multiplication is added

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ def add_(x, y):
     print("\nadd of", x, "+", y, "=", x + y)
 def sub_(x, y):
     print("\nsub of", x, "-", y, "=", x - y)
+def mul_(x, y):
+    print("\nmull of", x, "*", y, "=", x * y)
 
 while exit == 0:
     print(" \n ------- simple calculator ------- \n ")
-    fun = int(input("Choose a function \n 1 : add \n 2 : sub \n 0 : exit \n"))
+    fun = int(input("Choose a function \n 1 : add \n 2 : sub  \n 3 : mul \n 0 : exit \n"))
     if fun == 0:
         break
     x = int(input("Enter 1st number"))
@@ -15,6 +17,8 @@ while exit == 0:
         add_(x, y)
     elif fun == 2:
         sub_(x, y)
+    elif fun == 3:
+        mul_(x, y)
     elif fun == 0:
         break
     else :

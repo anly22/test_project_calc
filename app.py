4b33a2983299feba3bebb2d27269cdd83bef4820
anly22 <alysokhmara@gmail.com>
2024-02-23 12:27:18 +0200

    Division is added

--- app.py
+++ app.py
@@ -3,10 +3,15 @@ def add_(x, y):
     print("\nadd of", x, "+", y, "=", x + y)
 def sub_(x, y):
     print("\nsub of", x, "-", y, "=", x - y)
+def div_(x, y):
+    if y != 0:
+        print("\ndiv of", x, "/", y, "=", x / y)
+    else :
+        print("not defined")   
 
 while exit == 0:
     print(" \n ------- simple calculator ------- \n ")
-    fun = int(input("Choose a function \n 1 : add \n 2 : sub \n 0 : exit \n"))
+    fun = int(input("Choose a function \n 1 : add \n 2 : sub \n 4 : div \n 0 : exit \n"))
     if fun == 0:
         break
     x = int(input("Enter 1st number"))
@@ -15,6 +20,8 @@ while exit == 0:
         add_(x, y)
     elif fun == 2:
         sub_(x, y)
+    elif fun == 4:
+        div_(x, y)
     elif fun == 0:
         break
     else :
